5456d734bbc93f77c9644b4901af36a1eeffc959
Preetham <preetham.guduri1@gmail.com>
2026-09-21 00:08:21 -0500

    Added STM32 & ARM HAL and Driver support
    Currently in the process of creating a linker script to program stm32.

---
+++ src/stm32/Core/main.cpp
@@ -0,0 +1,14 @@
+#include "stm32f4xx_hal.h"
+
+UART_HandleTypeDef huart2; 
+
+extern "C" {
+    void SystemClock_Config(void); 
+    void HAL_UART_MspInit(UART_HandleTypeDef* huart);
+
+    void SysTick_Handler(void){
+        HAL_IncTick();
+    }
+}
+
+
